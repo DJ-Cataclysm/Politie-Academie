15ce6f7964533009461fc2a7beed451ed93724fa
maestroke <formaestroke@gmail.com>
2017-02-01 14:33:38 +0100

    virtual reality start

---
+++ VR Test/VR Test/Assets/WandControllerRight.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class WandControllerRight : MonoBehaviour {
+
+    private EVRButtonId gripButton = EVRButtonId.k_EButton_Grip;
+    public bool gripButtonDown = false;
+    public bool gripButtonUp = false;
+    public bool gripButtonPressed = false;
+
+
+    private EVRButtonId triggerButton = EVRButtonId.k_EButton_SteamVR_Trigger;
+    public bool triggerButtonDown = false;
+    public bool triggerButtonUp = false;
+    public bool triggerButtonPressed = false;
+
+
+
+
+    private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObject.index); } }
+    private SteamVR_TrackedObject trackedObject;
+
+    // Use this for initialization
+    void Start() {
+        trackedObject = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if(controller == null) {
+            Debug.Log("Controller not initialized");
+            return;
+        }
+
+        gripButtonDown = controller.GetPressDown(gripButton);
+        gripButtonPressed = controller.GetPress(gripButton);
+        gripButtonUp = controller.GetPressUp(gripButton);
+
+        triggerButtonDown = controller.GetPressDown(triggerButton);
+        triggerButtonPressed = controller.GetPress(triggerButton);
+        triggerButtonPressed = controller.GetPressUp(triggerButton);
+
+        if (gripButtonDown) {
+            Debug.Log("GripButton was just pressed");
+        }
+        if (gripButtonUp) {
+            Debug.Log("Gripbutton was just released");
+        }
+
+        if (triggerButtonDown) {
+            Debug.Log("triggerButton was just pressed");
+        }
+        if (triggerButtonUp) {
+            Debug.Log("triggerbutton was just released");
+        }
+
+    }
+}
---
+++ VR Test/VR Test/Assets/scripts/Controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+using Valve.VR;
+
+public class Controller : MonoBehaviour {
+    SteamVR_Controller.ButtonMask testdsds = new SteamVR_Controller.ButtonMask();
+
+    
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //SteamVR_Controller control = new SteamVR_Controller();
+	}
+}
