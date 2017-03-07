8160f8021ac774ebaa9dac12ed0bc9b007b54376
Stonebro <anne_zweers@live.nl>
2017-03-07 16:07:35 +0100

    Progress

--- BegeiderInteractie/Assets/Scripts/TeacherControls.cs
+++ BegeiderInteractie/Assets/Scripts/TeacherControls.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TeacherControls : MonoBehaviour {
 
     public GameObject target;
+    private bool testBool;
 
 	// Use this for initialization
 	void Start () {
@@ -14,5 +15,16 @@ public class TeacherControls : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.J)) target.gameObject.gameObject.GetComponent<Renderer>().material.color = Color.blue;
+        if (Input.GetKey(KeyCode.Alpha1)) testBool = true;
+        if (Input.GetKey(KeyCode.Alpha2)) testBool = false;
+        print(testBool);
+
+        //if (testBool) {
+        //    GameObject.Find("CubeCam").SetActive(false);
+        //    GameObject.Find("TargetCam").SetActive(true);
+        //} else {
+        //    GameObject.Find("CubeCam").SetActive(true);
+        //    GameObject.Find("TargetCam").SetActive(false);
+        //}
     }
 }
