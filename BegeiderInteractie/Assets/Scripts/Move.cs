e53bc155309af1948a80475c3332467dab708470
Stonebro <anne_zweers@live.nl>
2017-03-07 15:13:07 +0100

    Basic cams

---
+++ BegeiderInteractie/Assets/Scripts/Move.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public int crudetimer = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+
+        if (crudetimer < 150) {
+            GetComponent<Transform>().transform.position -= transform.right * Time.deltaTime * 5;
+        } else if (crudetimer <= 300) {
+            GetComponent<Transform>().transform.position -= transform.right * Time.deltaTime * -5;
+        } else {
+            crudetimer = 0;
+        }
+        crudetimer++;
+    }
+}
---
+++ BegeiderInteractie/Assets/Scripts/TeacherControls.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeacherControls : MonoBehaviour {
+
+    public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.J)) target.gameObject.gameObject.GetComponent<Renderer>().material.color = Color.blue;
+    }
+}
