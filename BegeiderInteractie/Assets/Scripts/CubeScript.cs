2b432efe8859bd79a3c23782faea9a23a9f52b2e
Stonebro <anne_zweers@live.nl>
2017-03-08 13:14:43 +0100

    Slowly becoming a proper prototype

---
+++ BegeiderInteractie/Assets/Scripts/CubeScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour {
+
+    public bool isIdle { get; set; }
+    private int crudetimer = 0;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isIdle) {
+            if (crudetimer < 80) {
+                GetComponent<Transform>().transform.position -= transform.right * Time.deltaTime * -5;
+            } else if (crudetimer <= 160) {
+                GetComponent<Transform>().transform.position -= transform.right * Time.deltaTime * 5;
+            } else {
+                crudetimer = 0;
+            }
+            crudetimer++;
+        }
+    }
+
+    public void ActionOne() {
+        this.transform.GetComponent<Renderer>().material.color = Color.green;
+    }
+}
--- BegeiderInteractie/Assets/Scripts/TeacherControls.cs
+++ BegeiderInteractie/Assets/Scripts/TeacherControls.cs
@@ -1,30 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TeacherControls : MonoBehaviour {
 
-    public GameObject target;
     private bool testBool;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+    //public GameObject cube;
+    public Camera cubeCam;
+    public CubeScript cube;
+
+    //public GameObject cylinder;
+    public Camera cylinderCam;
+    public CylinderScript cylinder;
+
+    private string target;
+    public Text currentCam;
+
+    private void Start() {
+        currentCam.text = "Current cam: Cylinder";
+    }
+
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.J)) target.gameObject.gameObject.GetComponent<Renderer>().material.color = Color.blue;
-        if (Input.GetKey(KeyCode.Alpha1)) testBool = true;
-        if (Input.GetKey(KeyCode.Alpha2)) testBool = false;
-        print(testBool);
+        if (Input.GetKeyDown(KeyCode.J)) ActionOne();
 
-        //if (testBool) {
-        //    GameObject.Find("CubeCam").SetActive(false);
-        //    GameObject.Find("TargetCam").SetActive(true);
-        //} else {
-        //    GameObject.Find("CubeCam").SetActive(true);
-        //    GameObject.Find("TargetCam").SetActive(false);
-        //}
+        if (Input.GetKey(KeyCode.Alpha1)) target = "cube";
+        if (Input.GetKey(KeyCode.Alpha2)) target = "cylinder";
+
+        if (target == "cube") {
+            cubeCam.gameObject.SetActive(true);
+            cylinderCam.gameObject.SetActive(false);
+            currentCam.text = "Current cam: Cube";
+        } else if (target == "cylinder") {
+            cubeCam.gameObject.SetActive(false);
+            cylinderCam.gameObject.SetActive(true);
+            currentCam.text = "Current cam: Cylinder";
+        }
+    }
+
+    public void ActionOne() {
+        switch (target) {
+            case "cube":
+            cube.isIdle = true;
+            cube.ActionOne();
+            break;
+            case "cylinder":
+            cylinder.isIdle = true;
+            cylinder.ActionOne();
+            break;
+        }
     }
 }
