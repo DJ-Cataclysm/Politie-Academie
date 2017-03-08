4baf09776e74990bb8cde4fa0df8e3b538e1440d
DTBakker <djurre.t.bakker@gmail.com>
2017-03-08 15:00:04 +0100

    TargetControls added

---
+++ Scenario/Assets/Scripts/CubeScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour {
+
+    public bool isMoving = true;
+    private int crudetimer = 0;
+
+    //public ActionOne actionOne;
+    //public ActionTwo actionTwo;
+
+	// Update is called once per frame
+	void Update () {
+        if (isMoving) {
+            if (crudetimer < 80) {
+                GetComponent<Transform>().transform.position -= transform.right * Time.deltaTime * -5;
+            } else if (crudetimer <= 160) {
+                GetComponent<Transform>().transform.position -= transform.right * Time.deltaTime * 5;
+            } else {
+                crudetimer = 0;
+            }
+            crudetimer++;
+        }
+    }
+
+    public void FireAction(string s) {
+        switch (s) {
+            case "j":
+            //actionOne.Action();
+            break;
+            case "k":
+            //actionTwo.Action();
+            break;
+            case "l":
+            break;
+            case "i":
+            break;
+        }
+    }
+}
