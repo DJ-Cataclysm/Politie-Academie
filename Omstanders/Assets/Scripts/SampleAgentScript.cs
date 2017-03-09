303954d64c31b1ed32264f5ec0db5d7ff638e4fd
Stonebro <anne_zweers@live.nl>
2017-03-09 14:59:51 +0100

    Prototype

---
+++ Omstanders/Assets/Scripts/SampleAgentScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SampleAgentScript : MonoBehaviour {
+
+    public GameObject target;
+    NavMeshAgent agent;
+
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        target = GameObject.Find("Landmark" + Random.Range(1, 4));
+    }
+	
+	void Update () {
+        agent.SetDestination(target.transform.position);
+        if (agent.remainingDistance < 1) {
+            target = GameObject.Find("Landmark" + Random.Range(1, 4));
+            agent.SetDestination(target.transform.position);
+        }
+	}
+}
---
+++ Omstanders/Assets/Scripts/Spawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+
+    public GameObject npc;
+
+	void Start () {
+        int amountSpawned = Random.Range(2, 10);
+        print(amountSpawned);
+
+        for (int i = 0; i < amountSpawned; i++) {
+            Instantiate(npc, new Vector3(i * Random.Range(1, 5), 1, i * Random.Range(1, 5)), Quaternion.identity);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
