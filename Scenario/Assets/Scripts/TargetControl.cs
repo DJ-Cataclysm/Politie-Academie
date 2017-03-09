86194bc6799b19b22bc67f75b7b8d496d4dede5e
maestroke <formaestroke@gmail.com>
2017-03-09 14:47:32 +0100

    code gecomment

--- Scenario/Assets/Scripts/TargetControl.cs
+++ Scenario/Assets/Scripts/TargetControl.cs
@@ -4,26 +4,21 @@ using UnityEngine;
 using System;
 
 public class TargetControl : MonoBehaviour {
-    //private Transform self;
-
-    private bool walkToTarget = false;
+    private bool walkToTarget = false; //whether or not the target moves towards you
 
 
-    public Transform target;
-    public float rotationSlerp;
-    public float maxMovementSpeed;
+    public Transform target;    //the object the target walks towards
+    public float rotationSlerp;     //how fast the target rotates
+    public float maxMovementSpeed;  //how fast the target walks in seconds
 
-    public float maxDistance;
+    public float maxDistance;       //how close the target can get to you before stopping
 
-    static Func<float, float, float> pythagoras = (a, b) => (float)Math.Sqrt(a * a + b * b);
-
-    Func<Vector3, Vector3, float> getDistance = (a, b) => pythagoras(pythagoras(a.x, b.x), pythagoras(a.z, b.z));
-
+    //gets the difference between 2 floats
     Func<float, float, float> getDelta = (a, b) => a - b;
 
     // Use this for initialization
     void Start() {
-        //self = GetComponent<Transform>();
+        //if the movementspeed of the target is equal or less than zero, set it to 1.5
         if (maxMovementSpeed <= 0) {
             maxMovementSpeed = 1.5f;
         }
@@ -31,29 +26,39 @@ public class TargetControl : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetKeyDown(KeyCode.Space)) walkToTarget = !walkToTarget;
+        if (Input.GetKeyDown(KeyCode.Space)) walkToTarget = !walkToTarget; //if you press space, it starts or stops walking to you
     }
 
     void FixedUpdate() {
+        //if space has been pressed, walk and rotate
         if (walkToTarget) {
+            //rotates the target
             rotateToTarget();
 
+            //gets the distance between you and the target
             float distanceToTarget = Vector3.Distance(target.transform.position, transform.position);
 
-            Debug.Log(distanceToTarget + " - " + maxDistance);
+            //Debug.Log(distanceToTarget + " - " + maxDistance);
 
+            //if the target is outside the maxDistance range, move closer
             if (distanceToTarget > maxDistance) {
+                //a float that holds how many seconds the target takes to get to you
                 float factor = distanceToTarget / maxMovementSpeed;
 
+                //the difference in X and Z between you and the target
                 float deltaX = getDelta(target.position.x, transform.position.x);
                 float deltaZ = getDelta(target.position.z, transform.position.z);
 
+                //gets how much units the target moves on the X-axis this second.
                 float distanceThisSecondX = deltaX / factor;
+                //gets how much units the target moves on the Y-axis this frame.
                 float distanceThisFrameX = distanceThisSecondX * Time.deltaTime;
 
+                //same as above but then for the Z-axis
                 float distanceThisSecondZ = deltaZ / factor;
                 float distanceThisFrameZ = distanceThisSecondZ * Time.deltaTime;
 
+                //moves the target
                 transform.position += new Vector3(distanceThisFrameX, 0, distanceThisFrameZ);
             }
         }
