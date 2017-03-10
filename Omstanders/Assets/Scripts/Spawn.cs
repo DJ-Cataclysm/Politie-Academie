dc656f9b5e34bb8db3f29ec181ccc9731e644645
038Games <038GamesVR@gmail.com>
2017-03-10 14:18:54 +0100

    Enemy added

--- Omstanders/Assets/Scripts/Spawn.cs
+++ Omstanders/Assets/Scripts/Spawn.cs
@@ -6,7 +6,9 @@ using UnityEngine.AI;
 public class Spawn : MonoBehaviour {
 
     public GameObject npc;
+    public GameObject enemy;
     public float amountSpawned = 40;
+    public float amountEnemySpawned = 1;
     public float amountIdleSpawned = 1;
 
     void Start () {
@@ -38,6 +40,29 @@ public class Spawn : MonoBehaviour {
 
         for (int i=0;i<amountIdleSpawned;i++) {
             Instantiate(npc, new Vector3(GameObject.Find("IdleSpawnpoint1").transform.position.x, 1, GameObject.Find("IdleSpawnpoint1").transform.position.z), Quaternion.identity);
+        }
+
+        for (int i = 0; i < amountEnemySpawned; i++) {
+            string spawnpoint = "Spawnpoint" + Random.Range(1, 5);
+
+            // Depending on the selected starting point, randomize either the X or the Z coordinate, while making sure that the randomized property falls within the range of the other spawnpoints.
+            // This causes them to spawn in a nice rectangle (or however the spawnpoints are set up)
+            if (spawnpoint == "Spawnpoint1" || spawnpoint == "Spawnpoint3")
+                Instantiate(enemy,
+                    new Vector3(
+                    Random.Range(GameObject.Find("Spawnpoint4").transform.position.x, GameObject.Find("Spawnpoint2").transform.position.x),
+                    1,
+                    GameObject.Find(spawnpoint).transform.position.z),
+                    Quaternion.identity);
+            else if (spawnpoint == "Spawnpoint2" || spawnpoint == "Spawnpoint4")
+                Instantiate(enemy, new Vector3(
+                    GameObject.Find(spawnpoint).transform.position.x,
+                    1,
+                    Random.Range(GameObject.Find("Spawnpoint1").transform.position.z, GameObject.Find("Spawnpoint3").transform.position.z)),
+                    Quaternion.identity);
+            // This 'else' should never be reached
+            else
+                print("Oopsie! Mistake!");
         }
     }
 }
