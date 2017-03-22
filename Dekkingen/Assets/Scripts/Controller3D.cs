6f1b539bbea403b106eec6cd38d80ed218840fe7
maestroke <formaestroke@gmail.com>
2017-03-22 12:30:46 +0100

    m

---
+++ Dekkingen/Assets/Scripts/Controller3D.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controller3D : MonoBehaviour {
+
+    //public TargetReloader targets;
+
+    //public Text accuracy;
+    //public Text points;
+
+    int shotsFired;
+    int shotsHit;
+
+    public GameObject raycastObject;
+    RaycastHit objectHit;
+
+    public AudioClip gunShoot;
+    public AudioClip reload;
+    public AudioClip click;
+
+    public GunCockAnim anim;
+    public ParticleSystem p;
+
+    int shotCount;
+    public int clipSize;
+
+    void Start() {
+        this.gameObject.AddComponent<AudioSource>();
+        this.shotCount = 0;
+    }
+
+    //private void FixedUpdate() {
+        //int accurate = shotsHit * 100 / shotsFired;
+        //accuracy.text = "Accuracy: " + accurate + "%. Schoten: " + shotsFired + ", doelen geraakt: " + shotsHit;
+    //}
+
+    public void FireGun() {
+        if (GetComponent<Animation>().IsPlaying("reload") == false) {
+            if (shotCount < clipSize) {
+                this.shotCount++;
+                shotsFired++;
+                CheckForHit();
+
+                // Play animation
+                this.GetComponent<AudioSource>().clip = gunShoot;
+                this.GetComponent<AudioSource>().Play();
+                anim.GunCock();
+                p.Play();
+                if (this.shotCount == clipSize)
+                    anim.GunEmpty();
+            }
+            else if (shotCount >= clipSize) {
+                this.GetComponent<AudioSource>().clip = click;
+                this.GetComponent<AudioSource>().Play();
+            }
+        }
+    }
+
+    public void ReloadGun() {
+        //if (this.shotCount == clipSize) {
+            this.shotCount = 0;
+            this.GetComponent<AudioSource>().clip = reload;
+            this.GetComponent<AudioSource>().Play();
+            anim.GunReload();
+        //}
+        //else print("Pistool is niet leeg");
+    }
+
+    public void CheckForHit() {
+        Vector3 fwd = raycastObject.transform.TransformDirection(Vector3.forward);
+        Debug.DrawRay(raycastObject.transform.position, fwd * 50, Color.green);
+        if (Physics.Raycast(raycastObject.transform.position, fwd, out objectHit)) {
+            print(objectHit.transform.gameObject.name);
+                objectHit.transform.gameObject.SetActive(false);
+                //targets.targetHit = objectHit.transform;
+                shotsHit++;
+            //Destroy(objectHit.transform.gameObject);
+        }
+    }
+}
---
+++ Dekkingen/Assets/Scripts/TriggerAction.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TriggerAction : MonoBehaviour {
+    public HitCivilian hitCivilian;
+    public MissCivilian missCivilian;
+    public TurnToTransform turnToTransform;
+    public SampleAgentScript sampleAgentScript;
+
+    /// <summary>
+    /// !!!!!!!!!!!!!!! READ THIS BEFORE WORKING ON THIS SCRIPT!!!!!!!!!!!!!!!!!!!!
+    /// 
+    /// This script and its one function (FireAction) are used to trigger specific actions for the currently selected target.
+    /// (To be continued)
+    /// </summary>
+
+
+    public void FireAction(string s) {
+
+        switch (s) {
+            case "0":
+                // Total reset
+                break;
+            case "1":
+                hitCivilian.shootAtCivilian();
+                GetComponent<NavMeshAgent>().enabled = false;
+                GetComponent<SampleAgentScript>().enabled = false;
+                break;
+            case "2":
+                missCivilian.shootAtCivilian();
+                GetComponent<NavMeshAgent>().enabled = false;
+                GetComponent<SampleAgentScript>().enabled = false;
+                break;
+            case "3":
+                GetComponent<NavMeshAgent>().enabled = true;
+                GetComponent<SampleAgentScript>().enabled = true;
+                sampleAgentScript.walkToPlayer = !sampleAgentScript.walkToPlayer;
+                if (sampleAgentScript.walkToPlayer) sampleAgentScript.WalkToPlayer(); else sampleAgentScript.walkToLandmark();
+                break;
+            case "4":
+                GetComponent<NavMeshAgent>().enabled = true;
+                GetComponent<SampleAgentScript>().enabled = true;
+                break;
+            case "5":
+                break;
+            case "6":
+                break;
+            case "7":
+                break;
+            case "8":
+                break;
+            case "9":
+                break;
+            default:
+                break;
+        }
+    }
+}
