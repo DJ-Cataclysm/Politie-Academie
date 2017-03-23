b6eec82b3756044e2b75674b072b926392ea5a1f
DTBakker <djurre.t.bakker@gmail.com>
2017-03-23 12:20:05 +0100

    Explosion update

--- Scenario/Assets/ExplosionForce.cs
+++ Scenario/Assets/ExplosionForce.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ExplosionForce : MonoBehaviour {
 
-    public float radius = 0F;
+    public float radius = 2000F;
     public float power = 500.0F;
 
     // Use this for initialization
@@ -18,14 +18,14 @@ public class ExplosionForce : MonoBehaviour {
         if (Input.GetKeyDown("e"))
         {
             Debug.Log("EXPLOOOOOOOOOOOOOOOOSION");
-            Vector3 explosionPos = transform.position;
+            Vector3 explosionPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
             Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
             foreach (Collider hit in colliders)
             {
                 Rigidbody rb = hit.GetComponent<Rigidbody>();
 
                 if (rb != null)
-                    rb.AddExplosionForce(power, explosionPos, radius, 3.0F);
+                    rb.AddExplosionForce(power, explosionPos, radius, 0.0F);
             }
         }
         
