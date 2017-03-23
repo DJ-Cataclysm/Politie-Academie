70c2f810ba7c00b6564eeb26e20804952d933f5b
Stonebro <anne_zweers@live.nl>
2017-03-23 12:10:13 +0100

    NPC overhaul
    De 'npcs'-List is weg en vervangen door een base class NPC met 3 Lists:
    _all, _friendlies en _hostiles.

---
+++ Scenario/Assets/Scripts/NPCs/FriendlyNPC.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FriendlyNPC : NPC {
+
+    [SerializeField] private int landmarkAmount;
+    NavMeshAgent agent;
+    GameObject target;
+
+
+    // Set the agent, and set a random first target
+    void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        target = GameObject.Find("Landmark" + Random.Range(1, landmarkAmount + 1));
+    }
+	
+	// Agent will move towards his destination, until he's close
+	void Update () {
+        agent.SetDestination(target.transform.position);
+        if (agent.remainingDistance < 2) {
+            // When he's close enough, find and set a new destination for the poor bugger
+            target = GameObject.Find("Landmark" + Random.Range(1, (landmarkAmount + 1)));
+            agent.SetDestination(target.transform.position); ;
+        }
+    }
+
+    protected override void OnDestroy() {
+        if (friendlies != null) {
+            friendlies.Remove(this);
+            all.Remove(this);
+        }
+        print("Deleted from list! " + this + "  friendlies is: " + friendlies.Count + "   all is: " + all.Count);
+    }
+}
--- Scenario/Assets/Scripts/NPCs/Spawn.cs
+++ Scenario/Assets/Scripts/NPCs/Spawn.cs
@@ -5,129 +5,70 @@ using UnityEngine.AI;
 
 public class Spawn : MonoBehaviour {
 
-    // Variables relating to the current target. These affect the Target Cam, and are determined by their index in the npcs List item.
-    private Transform currentTarget;
-    private int currentTargetIndex = 0;
+    [SerializeField] private Transform npc;
+    [SerializeField] private Transform enemy;
 
-    private int landmarkAmount = 0;
+    // Variables relating to the amount of NPCs to spawn. These can be altered from the menu. The values here are just defaults.
+    [SerializeField] private int amountNormalSpawned = 40;
+    [SerializeField] private int amountHostilesSpawned = 1;
+    [SerializeField] private int amountIdleSpawned = 0;
 
-    // Variables relating to the different types of NPCs, these will be used to spawn them.
-    private List<Transform> npcs = new List<Transform>();
-    private static List<Transform> npcsToTransfer = new List<Transform>();
-    public Transform npc;
-    public Transform enemy;
-
-    // Variables relating to the amount of NPCs to spawn. These can be altered from the menu.
-    private float amountToSpawn;
-    public float amountNormalSpawned = 40;
-    public float amountEnemySpawned = 0;
-    public float amountIdleSpawned = 0;
-
-    public List<AudioClip> clips = new List<AudioClip>();
-   
 
     void Start() {
-        foreach (Transform child in GameObject.Find("Landmarks").transform) landmarkAmount++;
+        SpawnFriendlies(amountNormalSpawned);
+        SpawnIdles(amountIdleSpawned);
+        SpawnHostiles(amountHostilesSpawned);
 
-        amountToSpawn = amountNormalSpawned + amountEnemySpawned + amountIdleSpawned;
-        print("Amount to spawn is: " + amountToSpawn + ". Normal is: " + amountNormalSpawned + ". Idle is: " + amountIdleSpawned + ". Enemy is: " + amountEnemySpawned);
-
-        for (int i = 0; i < amountToSpawn; i++) {
-            // For each npc to place, select a random starting point.
-            string spawnpoint = "Spawnpoint" + Random.Range(1, 5);
-
-            // Place the enemies first.
-            if (i < amountEnemySpawned) {
-                // Depending on the selected starting point, randomize either the X or the Z coordinate, while making sure that the randomized property falls within the range of the other spawnpoints.
-                // This causes them to spawn in a nice rectangle (or however the spawnpoints are set up)
-                if (spawnpoint == "Spawnpoint1" || spawnpoint == "Spawnpoint3")
-                    npcs.Add(Instantiate(enemy, new Vector3(
-                        Random.Range(GameObject.Find("Spawnpoint4").transform.position.x, GameObject.Find("Spawnpoint2").transform.position.x),
-                        1,
-                        GameObject.Find(spawnpoint).transform.position.z),
-                        Quaternion.identity, transform));
-
-                else if (spawnpoint == "Spawnpoint2" || spawnpoint == "Spawnpoint4")
-                    npcs.Add(Instantiate(enemy, new Vector3(
-                        GameObject.Find(spawnpoint).transform.position.x,
-                        1,
-                        Random.Range(GameObject.Find("Spawnpoint1").transform.position.z, GameObject.Find("Spawnpoint3").transform.position.z)),
-                        Quaternion.identity, transform));
-                npcs[i].GetComponent<SampleAgentScript>().landmarkAmount = landmarkAmount;
-                //npcs[i].GetComponent<SampleAgentScript>().player = GameObject.Find("Camera (head)").transform;
-
-                // After the enemies, spawn the idle NPCs in the same way
-            } else if (i >= amountEnemySpawned && i < (amountEnemySpawned + amountIdleSpawned)) {
-                npcs.Add(Instantiate(npc, new Vector3(
-                    GameObject.Find("IdleSpawnpoint" + (i - amountNormalSpawned + 1)).transform.position.x,
-                    1,
-                    GameObject.Find("IdleSpawnpoint" + (i - amountNormalSpawned + 1)).transform.position.z),
-                    Quaternion.identity, transform));
-                //SampleAgentScript test = npcs[i].GetComponent<SampleAgentScript>();
-                //test.isIdle = true;
-                Destroy(npcs[i].GetComponent<NavMeshAgent>());
-                Destroy(npcs[i].GetComponent<SampleAgentScript>());
-
-                npcs[i].GetComponent<SampleAgentScript>().landmarkAmount = landmarkAmount;
-                // After the idle NPCs, spawn the normal NPCs in the same way.
-            } else if (i >= (amountEnemySpawned + amountIdleSpawned) && i < amountToSpawn) {
-                if (spawnpoint == "Spawnpoint1" || spawnpoint == "Spawnpoint3")
-                    npcs.Add(Instantiate(npc,
-                        new Vector3(
-                        Random.Range(GameObject.Find("Spawnpoint4").transform.position.x, GameObject.Find("Spawnpoint2").transform.position.x),
-                        1,
-                        GameObject.Find(spawnpoint).transform.position.z),
-                        Quaternion.identity, transform));
-                else if (spawnpoint == "Spawnpoint2" || spawnpoint == "Spawnpoint4")
-                    npcs.Add(Instantiate(npc, new Vector3(
-                        GameObject.Find(spawnpoint).transform.position.x,
-                        1,
-                        Random.Range(GameObject.Find("Spawnpoint1").transform.position.z, GameObject.Find("Spawnpoint3").transform.position.z)),
-                        Quaternion.identity, transform));
-
-
-                npcs[i].GetComponent<SampleAgentScript>().landmarkAmount = landmarkAmount;
-            }
-        }
-
-        for (int i = (int)amountEnemySpawned; i < npcs.Count; i++) npcsToTransfer.Add(npcs[i]);
-
-
-
-        print("npcs.count: " + npcs.Count);
-        currentTarget = npcs[0];
-        for (int i = (int)amountEnemySpawned+1; i < amountToSpawn; i++)
-            if (i % 8 == 0) {
-                npcs[i].GetComponent<AudioSource>().enabled = true;
-                //npcs[i].GetComponent<AudioSource>().PlayOneShot();
-            }
+        print("All: " + NPC.all.Count + "  Friendly: " + NPC.friendlies.Count + "  Hostile: " + NPC.hostiles.Count);
     }
+
+    // This function decides where and how the standard Friendly NPCs spawn (Type: FriendlyNPC.cs)
+    private void SpawnFriendlies(int amountToSpawn) {
+        for (float i = 0; i < amountToSpawn; i++) {
+            string spawnpoint = "Spawnpoint" + Random.Range(1, 5);
+            if (spawnpoint == "Spawnpoint1" || spawnpoint == "Spawnpoint3")
+                Instantiate(npc,
+                    new Vector3(
+                    Random.Range(GameObject.Find("Spawnpoint4").transform.position.x, GameObject.Find("Spawnpoint2").transform.position.x),
+                    1,
+                    GameObject.Find(spawnpoint).transform.position.z),
+                    Quaternion.identity, transform);
+            else if (spawnpoint == "Spawnpoint2" || spawnpoint == "Spawnpoint4")
+                Instantiate(npc, new Vector3(
+                    GameObject.Find(spawnpoint).transform.position.x,
+                    1,
+                    Random.Range(GameObject.Find("Spawnpoint1").transform.position.z, GameObject.Find("Spawnpoint3").transform.position.z)),
+                    Quaternion.identity, transform);
+        }
+    }
+
+    // This function spawns Idle NPCs. It's empty right now..
+    private void SpawnIdles(int amountToSpawn) {
+
+    }
+
+    // This function decides where and how the Hostile NPCs spawn. (Type: HostileNPC.cs)
+    private void SpawnHostiles(int amountToSpawn) {
+        for (float i = 0; i < amountToSpawn; i++) {
+            string spawnpoint = "Spawnpoint" + Random.Range(1, 5);
+            if (spawnpoint == "Spawnpoint1" || spawnpoint == "Spawnpoint3")
+                Instantiate(enemy, new Vector3(
+                    Random.Range(GameObject.Find("Spawnpoint4").transform.position.x, GameObject.Find("Spawnpoint2").transform.position.x),
+                    1,
+                    GameObject.Find(spawnpoint).transform.position.z),
+                    Quaternion.identity, transform);
+
+            else if (spawnpoint == "Spawnpoint2" || spawnpoint == "Spawnpoint4")
+                Instantiate(enemy, new Vector3(
+                    GameObject.Find(spawnpoint).transform.position.x,
+                    1,
+                    Random.Range(GameObject.Find("Spawnpoint1").transform.position.z, GameObject.Find("Spawnpoint3").transform.position.z)),
+                    Quaternion.identity, transform);
+        }
+    }
+
+
 
     private void Update() {
-        // If the keys "1", "2", "3", or "4" is pressed, that key is sent to the current target's script. Depending on the target, they (could) act differently.
-        //if(Input.anyKeyDown)
-        if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Alpha4))
-            currentTarget.GetComponent<TriggerAction>().FireAction(Input.inputString);
-
-        // If LeftArrow or RightArrow are pressed, shut off the current target's camera, advance to the next target, and switch on its camera.
-        if (Input.GetKeyDown(KeyCode.RightArrow)) {
-            currentTarget.GetChild(0).transform.gameObject.SetActive(false);
-            currentTargetIndex++;
-            // A simple "if" to prevent the currentTargetIndex from going out of bounds
-            if (currentTargetIndex > (amountEnemySpawned - 1)) currentTargetIndex = 0;
-        }
-        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
-            currentTarget.GetChild(0).transform.gameObject.SetActive(false);
-            currentTargetIndex--;
-            if (currentTargetIndex < 0) currentTargetIndex = (int)(amountEnemySpawned - 1);
-        }
-
-        // Set the currentTarget, and turn its camera on.
-        currentTarget = npcs[currentTargetIndex];
-        currentTarget.GetChild(0).transform.gameObject.SetActive(true);
-    }
-
-    public static List<Transform> getFriendlyNpcList() {
-        return npcsToTransfer;
     }
 }
