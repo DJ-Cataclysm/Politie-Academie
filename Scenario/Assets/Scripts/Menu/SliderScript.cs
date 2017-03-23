1724a2cf9c6b1ffa7db9c6931850a87ec6861689
Derpeus <m.b.pattipeilohy@gmail.com>
2017-03-23 14:35:23 +0100

    sound and weather sliders working

--- Scenario/Assets/Scripts/Menu/SliderScript.cs
+++ Scenario/Assets/Scripts/Menu/SliderScript.cs
@@ -7,9 +7,20 @@ using UnityEngine.UI;
 public class SliderScript : MonoBehaviour {
     public Slider slider;
     public static int dayNight;
+    public static int weatherSwitch;
   
+    public void setWeather() {
+        weatherSwitch = (int)slider.value;
+        print("Weather value: " + weatherSwitch);
+    }
+
+    public void setVolume() {
+        AudioListener.volume = slider.value;
+    }
+
+
     public void setDayNight() {
-        dayNight = (int)slider.value;
-        print("DayNight value: " + dayNight);
+            dayNight = (int)slider.value;
+            print("DayNight value: " + dayNight);
     }
 }
