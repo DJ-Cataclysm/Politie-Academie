9e8dfec23a0979a0c8f2ab0b7a4d74419ae9a568
maestroke <formaestroke@gmail.com>
2017-03-27 14:29:37 +0200

    m

--- Dekkingen + Scenario/Assets/Scripts/NPCs/FriendlyNPC.cs
+++ Dekkingen + Scenario/Assets/Scripts/NPCs/FriendlyNPC.cs
@@ -46,9 +46,9 @@ public class FriendlyNPC : NPC {
         }
 
         //Debug.Log(animator.GetCurrentAnimatorStateInfo(0).IsName("walking_inPlace"));
-        if (agent.speed < 5 && (animator.GetCurrentAnimatorStateInfo(0).IsName("walking_inPlace") || animator.GetCurrentAnimatorStateInfo(0).IsName("walking_inPlace 0"))) {
-            Debug.Log("test");
-        } 
+        //if (agent.speed < 5 && (animator.GetCurrentAnimatorStateInfo(0).IsName("walking_inPlace") || animator.GetCurrentAnimatorStateInfo(0).IsName("walking_inPlace 0"))) {
+            //Debug.Log("test");
+        //} 
 
     }
 
