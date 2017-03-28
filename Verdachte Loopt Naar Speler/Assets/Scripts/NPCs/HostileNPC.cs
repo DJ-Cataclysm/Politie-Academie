fb0981a30f235bf5d5fc7aaae9f52dfd790e35e6
maestroke <formaestroke@gmail.com>
2017-03-28 12:19:51 +0200

    loopt nu naar de speler toe

---
+++ Verdachte Loopt Naar Speler/Assets/Scripts/NPCs/HostileNPC.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class HostileNPC : NPC {
+
+    [SerializeField] private int landmarkAmount;
+    NavMeshAgent agent;
+    GameObject target;
+
+    public bool targetPlayer = false;
+
+    // Use this for initialization
+    void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        target = GameObject.Find("Landmark" + Random.Range(1, landmarkAmount + 1));
+        agent.SetDestination(target.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (agent.remainingDistance < 2 && !targetPlayer) {
+            // When he's close enough, find and set a new destination for the poor bugger
+            target = GameObject.Find("Landmark" + Random.Range(1, (landmarkAmount + 1)));
+            agent.SetDestination(target.transform.position);
+        }
+    }
+
+    private void OnEnable() {
+        targetPlayer = false;
+        target = GameObject.Find("Landmark" + Random.Range(1, (landmarkAmount + 1)));
+        agent.SetDestination(target.transform.position);
+    }
+
+    protected override void OnDestroy() {
+        if (hostiles != null) {
+            hostiles.Remove(this);
+            all.Remove(this);
+        }
+        print("Deleted from list! " + this + "  hostiles is: " + hostiles.Count + "   all is: " + all.Count);
+    }
+}
---
+++ Verdachte Loopt Naar Speler/Assets/Scripts/NPCs/TriggerAction.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TriggerAction : MonoBehaviour {
+    public HitCivilian hitCivilian;
+    public MissCivilian missCivilian;
+    public TurnToTransform turnToTransform;
+
+    private Transform player;
+
+    private NavMeshAgent agent {
+        get {
+            return GetComponent<NavMeshAgent>();
+        }
+    }
+
+    private HostileNPC hostileNPC {
+        get {
+            return GetComponent<HostileNPC>();
+        }
+    }
+
+    /// <summary>
+    /// !!!!!!!!!!!!!!! READ THIS BEFORE WORKING ON THIS SCRIPT!!!!!!!!!!!!!!!!!!!!
+    /// 
+    /// This script and its one function (FireAction) are used to trigger specific actions for the currently selected target.
+    /// (To be continued)
+    /// </summary>
+    /// 
+    private void Start() {
+        player = GameObject.Find("Player").transform;
+    }
+
+    public static TriggerAction CreateTriggerAction() {
+        return new TriggerAction();
+    }
+
+    public void FireAction(string s) {
+
+        switch (s) {
+            case "0":
+                // Total reset
+                break;
+            case "1":
+                hitCivilian.shootAtCivilian();
+                agent.enabled = false;
+                hostileNPC.enabled = false;
+                Invoke("Panic", 1.5f);
+                break;
+            case "2":
+                missCivilian.shootAtCivilian();
+                agent.enabled = false;
+                hostileNPC.enabled = false;
+                Invoke("Panic", 1.5f);
+                break;
+            case "3":
+                agent.enabled = true;
+                hostileNPC.enabled = true;
+                agent.destination = player.GetChild(2).transform.position;
+                hostileNPC.targetPlayer = true;
+                break;
+            case "4":
+                agent.enabled = true;
+                hostileNPC.enabled = true;
+                break;
+            case "5":
+                Panic();
+                break;
+            case "6":
+                break;
+            case "7":
+                break;
+            case "8":
+                break;
+            case "9":
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void Panic() { // at the Disco
+        foreach (FriendlyNPC child in NPC.friendlies) child.Panic();
+    }
+}
